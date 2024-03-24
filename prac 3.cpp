d1d93cafbd7b760cf2a13a035e3c29a103e4ae41
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-03-24 15:51:34 +0530

    Update prac 3.cpp

--- prac 3.cpp
+++ prac 3.cpp
@@ -1,29 +1,26 @@
 /* write a program that prints the table indicating the number of occurences of each alphabet in a text entered as a command line arguments */
 #include <iostream>
-#include <cstring>
+#include<string>
 using namespace std;
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " <text>" << endl;
-        return 1;
-    }
-    int counts[26] = {0};
-    for (int i = 1; i < argc; ++i) {
-        for (int j = 0; j < strlen(argv[i]); ++j) {
-            char c = argv[i][j];
-            if (isalpha(c)) {
-                c = tolower(c);
-                counts[c - 'a']++;
+int main() {
+    string str;
+    cout<<" enter line of string: ";
+    getline(cin,str);
+    int count =0;
+    for(int i=0;i<str.length();i++){
+        count =0;
+        for(int j=0;j<str.length();j++){
+            if(j<i && str[i]==str[j]){
+                break;
+            }
+            if(str[i]==str[j]){
+                count++;
             }
         }
+        if(count != 0){
+        cout<<str[i]<<" used number of time"<<count<<endl;
+        }
     }
-
-    // Print the counts of each alphabet
-    cout << "Alphabet Counts:" << endl;
-    for (int i = 0; i < 26; ++i) {
-        cout << static_cast<char>('a' + i) << ": " << counts[i] << endl;
-    }
-
     return 0;
 }
