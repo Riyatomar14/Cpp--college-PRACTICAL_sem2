d51fcb0c0a13993eed31dbd7bbb60b0671f8cb13
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-03-27 20:13:26 +0530

    Update prac7.cpp

--- prac7.cpp
+++ prac7.cpp
@@ -1,7 +1,32 @@
 /* gcd with and without recursion */
 
+// recursion
+
 #include <iostream>
-int gcd(int a, int b) {
+using namespace std;
+int gcd(int a,int b){
+    if(a%b==0){
+        return b;
+    }
+    return gcd(b,a%b);
+}
+int main() {
+    int num1,num2,result;
+    cout<<"enter number1:";
+    cin>>num1;
+    cout<<"enter number2:";
+    cin>>num2;
+    result=gcd(num1,num2);
+    cout<<"GCD:"<<result;
+    return 0;
+}
+
+// without recursion
+
+#include <iostream>
+using namespace std;
+
+int gcdNonRecursive(int a, int b) {
     while (b != 0) {
         int temp = b;
         b = a % b;
@@ -9,19 +34,15 @@ int gcd(int a, int b) {
     }
     return a;
 }
-int gcdRecursive(int a, int b) {
-    if (b == 0) {
-        return a;
-    }
-    return gcdRecursive(b, a % b);
-}
 
 int main() {
-    int num1 = 24, num2 = 36;
-    int result = gcd(num1, num2);
-    std::cout << "GCD of " << num1 << " and " << num2 << " is " << result << std::endl;
-    result = gcdRecursive(num1, num2);
-    std::cout << "GCD of " << num1 << " and " << num2 << " is " << result << std::endl;
-
+    int a;
+    int b;
+    cout<<"enter number1:";
+    cin>>a;
+    cout<<"enter number2:";
+    cin>>b;
+    cout << "GCD of " << a << " and " << b << " is: " << gcdNonRecursive(a, b) << endl;
     return 0;
 }
+
