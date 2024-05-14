7cdfd585a04dd3066d8d780a272c2b09004ad370
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-05-14 12:42:49 +0530

    Update prac6.cpp

--- prac6.cpp
+++ prac6.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int size, int key) {
+int binarySearchNon_Recursion(int arr[], int size, int key) {
     int high = size - 1;
     int low = 0;
     while (low <= high) {
@@ -20,24 +20,8 @@ int binarySearch(int arr[], int size, int key) {
     return -1;
 }
 
-int main() {
-    int arr[] = {1, 3, 6, 8, 9, 10, 15, 34, 54, 56, 78};
-    int size = sizeof(arr) / sizeof(arr[0]); 
-    int key = 8;
-    int result = binarySearch(arr, size, key);
-    if (result != -1) {
-        cout << "Element found at index " << result << endl;
-    } else {
-        cout << "Element not found" << endl;
-    }
-    return 0;
-}
 
-// Binary search with recursion
-#include <iostream>
-using namespace std;
-
-int binarySearch(int arr[], int key, int high, int low) {
+int binarySearchRecursion(int arr[], int key, int high, int low) {
     if (low > high) return -1;
     
     int mid = (high + low) / 2;
@@ -45,20 +29,27 @@ int binarySearch(int arr[], int key, int high, int low) {
         return mid;
     }
     else if (key > arr[mid]) {
-        return binarySearch(arr, key, high, mid + 1);
+        return binarySearchRecursion(arr, key, high, mid + 1);
     }
     else {
-        return binarySearch(arr, key, mid - 1, low);
+        return binarySearchRecursion(arr, key, mid - 1, low);
     }
 }
 
 int main() {
     int arr[] = {1, 3, 6, 8, 9, 10, 15, 34, 54, 56, 78};
-    int n = sizeof(arr) / sizeof(arr[0]); 
+    int size = sizeof(arr) / sizeof(arr[0]); 
     int key = 8;
-    int high = n - 1; 
+    int result = binarySearchNon_Recursion(arr, size , key);
+    if (result != -1) {
+        cout << "Element found at index " << result << endl;
+    } else {
+        cout << "Element not found" << endl;
+    }
+    
+    int high = size - 1; 
     int low = 0;
-    int result = binarySearch(arr, key, high, low);
+    int Result = binarySearchRecursion( arr, key, high ,low);
     if (result != -1) {
         cout << "Element found at index " << result << endl;
     } else {
@@ -66,5 +57,3 @@ int main() {
     } 
     return 0;
 }
-
-
