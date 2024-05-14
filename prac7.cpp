80203e906efd760ffbc40da1db3a8015dd996ea6
Riya Tomar <143107173+Riyatomar14@users.noreply.github.com>
2024-05-14 12:51:40 +0530

    Update prac7.cpp

--- prac7.cpp
+++ prac7.cpp
@@ -4,28 +4,13 @@
 
 #include <iostream>
 using namespace std;
+
 int gcd(int a,int b){
     if(a%b==0){
         return b;
     }
     return gcd(b,a%b);
 }
-int main() {
-    int num1,num2,result;
-    cout<<"enter number1:";
-    cin>>num1;
-    cout<<"enter number2:";
-    cin>>num2;
-    result=gcd(num1,num2);
-    cout<<"GCD:"<<result;
-    return 0;
-}
-
-// without recursion
-
-#include <iostream>
-using namespace std;
-
 int gcdNonRecursive(int a, int b) {
     while (b != 0) {
         int temp = b;
@@ -42,7 +27,8 @@ int main() {
     cin>>a;
     cout<<"enter number2:";
     cin>>b;
+    result=gcd(num1,num2);
+    cout<<"GCD:"<<result;
     cout << "GCD of " << a << " and " << b << " is: " << gcdNonRecursive(a, b) << endl;
     return 0;
 }
-
